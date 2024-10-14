7320550e38f4acbc02263dcf2a12e1124a6db45c
tanish0510 <tanishseth0510@gmail.com>
2024-10-14 11:58:28 +0530

    new

--- ecofinance/backend/models/greenscore.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const GreenScoreSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    score: { type: Number, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('GreenScore', GreenScoreSchema);
